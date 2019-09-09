51574a283272a20a692bb669194fe65671922465
razielar <razielar@gmail.com>
2019-09-09 18:58:29 +0200

    Put all options and libraries

--- GO_enrichment.R
+++ GO_enrichment.R
@@ -5,6 +5,8 @@
 ##### Libraries:
 suppressPackageStartupMessages(library(optparse))
 suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(GO.db))
+suppressPackageStartupMessages(library(GOstats))
 options(stringsAsFactors = FALSE)
 
 ##### Option list using Python's style: 
@@ -14,9 +16,12 @@ option_list <- list(
   make_option(c("-u", "--universe"), help = "a list of gene identifiers, NO header"),
   make_option(c("-G", "--genes"), default = "stdin",
               help = "a list of gene identifiers for the foreground, NO header [default=%default]"),
-  make_option(c("-o", "--ontology"), help = "choose the Gene Ontology < BP,MF,CC > [default=%default]", 
-              default= "BP")
-  
+  make_option(c("-O", "--ontology"), help = "choose the Gene Ontology < (BP,MF,CC) > [default=%default]", 
+              default= "BP"),
+  make_option(c("-s", "--species"), help = "choose the species <(Dmel, Human)> [default=%default]",
+              default = "Dme"),
+  make_option(c("-o", "--output"), help = "name of the output [default=%default]", 
+              default = "GO.enrich.txt")
 )
 
 opt_parser <- OptionParser(option_list = option_list)
