54e352c73bf78ac83eb1361f934fb4e99afc8b0e
Raziel Amador <razielar@gmail.com>
2019-09-19 17:11:32 +0200

    try human

--- GO_enrichment.R
+++ GO_enrichment.R
@@ -13,7 +13,7 @@ options(stringsAsFactors = FALSE)
 
 ##### Option list using Python's style: 
 option_list <- list(
-  make_option(c("-u", "--universe"), help = "a list of gene identifiers, WITH header"),
+  make_option(c("-u", "--universe"), help = "a list of gene identifiers, NO header"),
   make_option(c("-G", "--genes"), default = "stdin",
               help = "a list of gene identifiers for the foreground, NO header [default=%default]"),
   make_option(c("-O", "--ontology"), help = "choose the Gene Ontology <(BP,MF,CC)> [default=%default]", 
@@ -49,7 +49,7 @@ if(opt$species == "Human"){
 # G <- read.delim("Documents/Home.office.2019/GO.adipose.omentum.txt")
 ######### Debuggin purposes
 #Information regarding Gene_Universe: https://www.researchgate.net/post/How_do_you_perform_a_gene_ontology_with_topGO_in_R_with_a_predefined_gene_list
-U <- read.delim(opt$universe, col.names = "hs")
+U <- read.delim(opt$universe, col.names = "hs", header = FALSE)
 U$hs <- unique(U$hs)
 
 if(opt$genes == "stdin"){
@@ -75,75 +75,67 @@ if(opt$species == "Human"){
 }
 
 # Take the Flybase IDs for all orthologous genes which will be my universe
-if(opt$species == "Dme"){
-  universe <- unlist(mget(U$hs, org.Dm.egENSEMBL2EG, ifnotfound = NA))
-}
-
-if(opt$species == "Human"){
-  universe <- unlist(mget(U$hs, org.Hs.egENSEMBL2EG, ifnotfound = NA))
-}
-
-sprintf("%s background genes; with %s with a corresponding ID", nrow(U), 
-        length(unique(universe)))
-
- 
-createParams <- function(x, species= "Dme"){
-  
-  if(species == "Dme"){
-    geneset <- unlist(mget(x, org.Dm.egENSEMBL2EG, ifnotfound = NA))
-  }
-  # print(length(x), length(unique(geneset)))
-  sprintf("%s foreground genes; %s with a corresponding entrez id", length(x),
-          length(unique(geneset)))
-  # pv <- 1-(1-0.05)**length(x)
-  # print(pv)
-  params <- new("GOHyperGParams",
-                geneIds= geneset,
-                universeGeneIds=universe,
-                annotation=ann,
-                ontology= opt$ontology,
-                pvalueCutoff= 0.01,
-                conditional= TRUE,
-                testDirection="over")
-  
-  return(params)
-}
-
-res <- suppressWarnings(hyperGTest(createParams(unique(G$hs), opt$species)))
-cat("Finished Hypergeometric test", "\n")
-res 
-
-# Reformat the output table: 
-df <- summary(res)
-
-# df$Pvalue <- round(df$Pvalue, digits = 2)
-df$OddsRatio <- round(df$OddsRatio, 2)
-df$ExpCount <- round(df$ExpCount, 2)
-
-df <- df[df$Count >= 3,]
-print(df)
-
+# if(opt$species == "Dme"){
+#   universe <- unlist(mget(U$hs, org.Dm.egENSEMBL2EG, ifnotfound = NA))
+# }
 # 
-# htmlReport(res)
-
-# Get the genes for enriched GO terms
-
-enrichGenes <- lapply(geneIdsByCategory(res, catids=sigCategories(res, pvalueCutoff(res))), 
-                      data.frame)
-
-length(enrichGenes)
-# enrichGenes[[2]] <- mapIds(eval(parse(text=ann)), keys=enrichGenes[[2]], 
-#                            keytype="ENTREZID", column=c("ENSEMBL"))
-# colnames(enrichGenes) <- c("GO", "gene_id")
+# if(opt$species == "Human"){
+#   universe <- unlist(mget(U$hs, org.Hs.egENSEMBL2EG, ifnotfound = NA))
+# }
 # 
-# print(enrichGenes)
-
-
-
-
-
-
-
-
-
+# sprintf("%s background genes; with %s with a corresponding ID", nrow(U), 
+#         length(unique(universe)))
+# 
+#  
+# createParams <- function(x, species= "Dme"){
+#   
+#   if(species == "Dme"){
+#     geneset <- unlist(mget(x, org.Dm.egENSEMBL2EG, ifnotfound = NA))
+#   }
+#   # print(length(x), length(unique(geneset)))
+#   sprintf("%s foreground genes; %s with a corresponding entrez id", length(x),
+#           length(unique(geneset)))
+#   # pv <- 1-(1-0.05)**length(x)
+#   # print(pv)
+#   params <- new("GOHyperGParams",
+#                 geneIds= geneset,
+#                 universeGeneIds=universe,
+#                 annotation=ann,
+#                 ontology= opt$ontology,
+#                 pvalueCutoff= 0.01,
+#                 conditional= TRUE,
+#                 testDirection="over")
+#   
+#   return(params)
+# }
+# 
+# res <- suppressWarnings(hyperGTest(createParams(unique(G$hs), opt$species)))
+# cat("Finished Hypergeometric test", "\n")
+# res 
+# 
+# # Reformat the output table: 
+# df <- summary(res)
+# 
+# # df$Pvalue <- round(df$Pvalue, digits = 2)
+# df$OddsRatio <- round(df$OddsRatio, 2)
+# df$ExpCount <- round(df$ExpCount, 2)
+# 
+# df <- df[df$Count >= 3,]
+# print(df)
+# 
+# # 
+# # htmlReport(res)
+# 
+# # Get the genes for enriched GO terms
+# 
+# enrichGenes <- lapply(geneIdsByCategory(res, catids=sigCategories(res, pvalueCutoff(res))), 
+#                       data.frame)
+# 
+# length(enrichGenes)
+# # enrichGenes[[2]] <- mapIds(eval(parse(text=ann)), keys=enrichGenes[[2]], 
+# #                            keytype="ENTREZID", column=c("ENSEMBL"))
+# # colnames(enrichGenes) <- c("GO", "gene_id")
+# # 
+# # print(enrichGenes)
+# 
 
